8882eaec583806079ff89983db9aa92d462fff5f
alex <AlexKomzzz>
2022-09-26 00:07:55 +0300

    струтурирование

--- cmd/server.go
+++ cmd/server.go
@@ -4,45 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-type ConnectUser struct {
-	Websocket *websocket.Conn
-	ClientIP  string
-}
-
-func newConnectUser(ws *websocket.Conn, clientIP string) *ConnectUser {
-	return &ConnectUser{
-		Websocket: ws,
-		ClientIP:  clientIP,
-	}
-}
-
-var clients = make(map[ConnectUser]int)
-
-//var clients map[*websocket.Conn]bool
-
-var upgrader = websocket.Upgrader{
-	// CheckOrigin: func(r *http.Request) bool {
-	// 	return true // пропускаем все
-	// },
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
+const (
+	host = "localhost:8080"
+)
 
 type Server struct {
 	clients map[*websocket.Conn]bool
 	//handleMessage func(message []byte)
 }
 
-func (server *Server) StartServer(handleMessage func(message []byte)) {
+func (server *Server) StartServer() {
 	http.Handle("/", http.FileServer(http.Dir("./web")))
 	// http.HandleFunc("/", server.IndexHandler)
 	http.HandleFunc("/ws", server.WebsocketHandler)
-	http.ListenAndServe("localhost:8080", nil)
+	log.Println("сервер запущен на хосту: ", host)
+	http.ListenAndServe(host, nil)
 }
 
 // func (server *Server) IndexHandler(w http.ResponseWriter, r *http.Request) {
@@ -52,45 +32,7 @@ func (server *Server) StartServer(handleMessage func(message []byte)) {
 // 	}
 // }
 
-func (server *Server) WebsocketHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatalln("error connect: ", err)
-	}
-	defer conn.Close()
-
-	log.Println("Client connected:", conn.RemoteAddr().String())
-	var socketClient *ConnectUser = newConnectUser(conn, conn.RemoteAddr().String())
-	// сохраняем соединение
-	//clients[conn] = true
-	clients[*socketClient] = 0
-	defer delete(clients, *socketClient)
-
-	for {
-		mtype, message, err := conn.ReadMessage() // читаем сообщение
-		if err != nil || mtype == websocket.CloseMessage {
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				log.Fatalf("error read message: %v", err)
-			}
-			break
-		}
-
-		timeMessage := time.Now().Format(time.Stamp)
-		message = []byte(fmt.Sprintf("%s %s\t\t%s", socketClient.ClientIP, timeMessage, string(message)))
-
-		go server.MyWriteMessage(message) // отправляем сообщение
-		// go messageHandler(message) // выводим сообщение
-		messageHandler(message)
-	}
-}
-
-func (server *Server) MyWriteMessage(message []byte) {
-	for user := range clients {
-		user.Websocket.WriteMessage(websocket.TextMessage, message)
-	}
-}
-
-func messageHandler(message []byte) {
+func messageHandler(message string) {
 	fmt.Println(string(message))
 }
 
@@ -99,7 +41,7 @@ func main() {
 		clients: make(map[*websocket.Conn]bool),
 		//handleMessage: handleMessage,
 	}
-	server.StartServer(messageHandler)
+	server.StartServer()
 
 	// for {
 	//server.MyWriteMessage([]byte("Hello"))
